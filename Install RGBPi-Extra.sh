1736dc344911f5409c1e5990ce6e56b53e079975
sd2cv <99993735+sd2cv@users.noreply.github.com>
2024-03-31 13:40:33 -0700

    Update Install RGBPi-Extra.sh

--- Install RGBPi-Extra.sh
+++ Install RGBPi-Extra.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+timedatectl set-timezone UTC
+timedatectl set-ntp true
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 script_url="https://raw.githubusercontent.com/sd2cv/RGBPi-Extra/main/RGBPi-Extra/application/installer.py"
 wget -q -O "$script_dir/installer.py" "$script_url"
